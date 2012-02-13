429338a32d7f0f927a258e0b203e286813b85af2
Sławosz <slawosz@gmail.com>
2012-02-13 20:03:36 +0100

    docs

--- app/helpers/rentals_helper.rb
+++ app/helpers/rentals_helper.rb
@@ -1,14 +1,17 @@
 module RentalsHelper
 
+  # dont render field for daily_fee which is set by after_save model callback
   def form_attributes_keys
     super - [:daily_fee]
   end
 
+  # add attribute which represents model instance methods
   def show_attributes
     hash = {:total_cost => {:display => :simple}}
     super.merge(hash)
   end
 
+  # this method returs array which keys to attributes in show action
   def show_attributes_keys
     super + [:total_cost]
   end
